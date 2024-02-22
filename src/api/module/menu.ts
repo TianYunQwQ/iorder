5a522330566829970f24854020b43aebc8c8ec5e
Tianyun Zhen <tianyunz@kth.se>
2024-02-22 23:33:24 +0100

    feature(custom): add \"addmenu\" feature

--- src/api/module/menu.ts
+++ src/api/module/menu.ts
@@ -1,7 +1,17 @@
 import request from '@/utils/request.js';
+import axios from 'axios';
 
 export const getMenu = () =>
   request({
     url: 'https://yapi.pro/mock/252699/api/menu',
     method: 'get'
   });
+export const postMenu = (values: any) => axios.post(
+  "https://my-json-server.typicode.com/TianYunQwQ/JSONPlaceholder/posts",
+  values,
+  {
+    headers: {
+      "Content-Type": "application/json", // 设置请求头为 application/json
+    },
+  }
+);
--- src/views/restaurantMenu/index.tsx
+++ src/views/restaurantMenu/index.tsx
@@ -2,10 +2,15 @@ import api from "@/api";
 import { useMenuStore, useOrderList } from "@/store";
 import { useEffect } from "react";
 import { Image, List, Button, Dialog } from 'antd-mobile'
+import { useNavigate } from 'react-router-dom'
 
 const ResMenu: React.FC = () => {
     const {menuList, setMenu} = useMenuStore()
     const { setOrderList} = useOrderList()
+    const navigate = useNavigate()
+    const Addmenu = ()=>{
+      navigate('/addmenu')
+    }
     useEffect(() => {
         const getMenuData = async() => {
         try {
@@ -18,47 +23,7 @@ const ResMenu: React.FC = () => {
         }
         getMenuData()
     },[setMenu])
-
-    const buttonStyle = {
-      borderRadius: '10px',
-      backgroundColor: 'orange',
-      padding: '10px',
-      width: '100%', // Set the width to 100% for equal sizes
-    };
     return (
-        // <div style={{position: 'relative', minHeight: '100vh'}}>
-        //   <em>
-        //     <strong>
-        //       <h1>Menu</h1>
-        //     </strong>
-        //   </em>
-  
-        //   {menuList.map((image, index) => (
-        //       <div key={index} className="menu-item" style={{display: 'flex', alignItems: 'center'}}>
-        //         <img
-        //             src={image.src}
-        //             title={image.name}
-        //             onTouchStart={() => alert(image.name)}
-        //             style={{width: '200px', marginRight: '10px', borderRadius: '20px'}}
-        //         />
-        //         <div>
-        //           <p>
-        //             <em>
-        //               <strong>{image.name}</strong>
-        //             </em>
-        //           </p>
-        //           <p style={{maxWidth: '120px' /* Set your desired maximum width here */}}>
-        //             {image.ingredients}
-        //           </p>
-        //           <p>
-        //             {image.price.toFixed(2)}kr<br/>
-        //             <div style={{display: 'flex', alignItems: 'center'}}>
-  
-        //             </div>
-        //           </p>
-        //         </div>
-        //       </div>
-        //   ))} 
         <div>
         <List header={ <span style={{ fontWeight: 'bold', fontStyle: 'italic', color: 'black', fontSize: '20px' }}>Menu</span>} style={{ width: '100%' , "--header-font-size":"20px", "--padding-right":"30px"}} mode='card' >
         {menuList.map(menuList => (
@@ -89,7 +54,7 @@ const ResMenu: React.FC = () => {
   
        
           <div style={{ marginTop: '20px', position: 'absolute', bottom: '60px', left: '20px' }}>
-            <Button color='warning' size='small' >Add</Button>
+            <Button color='warning' size='small' onClick={Addmenu}>Add</Button>
           </div>
   
           <div style={{ marginTop: '20px', position: 'absolute', bottom: '60px', right: '20px' }}>

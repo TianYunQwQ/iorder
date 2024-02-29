4b8bc11a436677810e6252a30b62a456b6ab7928
Tianyun Zhen <tianyunz@kth.se>
2024-02-29 02:50:31 +0100

    deploy(custom): deploy2

--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
 
 // production mock server (生产环境打包 mock 数据, 用于展示，正式生产务必删除！！！)
-if (process.env.NODE_ENV === 'production') {
-  import('./mockProdServer').then(({ setupProdMockServer }) => {
-    setupProdMockServer()
-  })
-}
+// if (process.env.NODE_ENV === 'production') {
+//   import('./mockProdServer').then(({ setupProdMockServer }) => {
+//     setupProdMockServer()
+//   })
+// }
